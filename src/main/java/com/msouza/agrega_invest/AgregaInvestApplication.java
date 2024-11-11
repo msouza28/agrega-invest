f049aa283587eb8e1d4c1346e7c25f6f2d1e7ece
msouza28 <matheus.emiliano28@gmail.com>
2024-11-11 13:31:42 -0300

    First commit

---
+++ src/main/java/com/msouza/agrega_invest/AgregaInvestApplication.java
@@ -0,0 +1,13 @@
+package com.msouza.agrega_invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregaInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregaInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msouza/agrega_invest/AgregaInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msouza.agrega_invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregaInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
